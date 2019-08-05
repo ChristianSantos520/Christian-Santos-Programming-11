7897359130ef2e6753a9e942951dccdfa6a2e16b
ChristianSantos520 <52550351+ChristianSantos520@users.noreply.github.com>
2019-08-05 16:59:53 -0700

    Update Main.java

--- ProjectNumber1/src/Main.java
+++ ProjectNumber1/src/Main.java
@@ -3,9 +3,13 @@ public class Main {
     static int problemOne(String s){
         int answer = 0;
 
+        //This array is used for comparing s if one of the characters in the array is a vowel.
         String[] vowels = {"a", "e", "i", "o", "u"};
 
         for (int i = 0; i < s.length(); i++){
+
+            //This for loop will go through all of the vowels to see if s.substring is a vowel.
+            //This for loop is runn every position in the s string.
             for (int j = 0; j < vowels.length; j++){
                 if (s.substring(i,i+1).equals(vowels[j])){
                     answer++;
@@ -22,6 +26,7 @@ public class Main {
     static int problemTwo(String s){
         int answer = 0;
 
+        //This for loop will run and check if s.substring has the string "bob".
         for (int i = 0; i < s.length() - 2; i++){
             if(s.substring(i,i+3).equals("bob")){
                 answer++;
@@ -35,21 +40,34 @@ public class Main {
     //Code your solution to problem number 3 here
     static String problemThree(String s){
 
+        //The position where the longest substring begins.
         int longestBeginning = 0;
 
+        //This variable tell you how long the longest substring is.
         int longestSize = 0;
 
+        //This is the beginning of the current substring.
         int probeBeginning = 0;
 
+        //This is the end of the current substring position
         int probeEnd = 0;
 
         for(int i = 0; i < s.length(); i++){
+
+            /*If the character at position 'i' is greater or equal to the end of the of the current substring, the
+            current position for the end of substring is equal to 'i'.  This will check if the substring will continue.
+            If the if statement is false, the substring substring will end.
+            */
             if(s.charAt(i) >= s.charAt(probeEnd)){
                 probeEnd = i;
 
             } else {
+                //This variable will determine the length is the current substring.
                 int probeLength = (probeEnd - probeBeginning + 1);
 
+                /*If the current substring is the greater that the last substring that was detected.  The longest
+                variables will be assiged by the probe variable because probe length is the longer substring.
+                */
                 if(probeLength > longestSize){
                     longestBeginning = probeBeginning;
                     longestSize = probeLength;
@@ -59,6 +77,7 @@ public class Main {
                 probeEnd = i;
             }
         }
+        //This will insure if the substring at the end of the s is the longest substring.
         int probeLength = (probeEnd - probeBeginning + 1);
         if(probeLength > longestSize){
             longestBeginning = probeBeginning;
